329017730e180b99284feccfd5de67e95a523c3c
soontobepydev <andre.97asd@hotmail.com>
2020-08-14 07:24:02 +0100

    added toggle player function corrected all functions prior. everything working smoothly added player index so we have player and computer

--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@
 
 import json
 import os
+import random
 
 
 def main():
@@ -21,8 +22,10 @@ def play_game(roles):
     p1 = get_player()
 
     winner = None
+    player_index = pick_first()
     while not winner:
-        role = get_imput(roles)
+        role = make_a_play(roles)
+        player_index = toggle_player(player_index)
         if check_winner():
             print(f"Congratulations, {winner}! you won!")
 
@@ -55,25 +58,50 @@ def get_player():
             print("You cannot put an empty name")
 
 
-def get_imput(roles):
-    role = None
-    
-    available_roles = [key for key in roles]
-    print(available_roles)
-    while role not in available_roles:
-        try:
+def pick_first():
+    player_input = ['me', '0']
+    computer_input = ['pc', '1']
+    random_input = ['rand', 'random']
+
+    pick = None
+
+    print(f'Who goes first? you or computer ({player_input[0]} / {computer_input[0]}) or ({player_input[1]}) '
+                 f'/ {computer_input[1]}) or even ({random_input[0]} / {random_input[1]})')
+    while pick not in player_input or pick not in computer_input or pick not in random_input:
+        pick = input('feel free to pick').strip()
+        if pick in player_input:
+            return 0
+        elif pick in computer_input:
+            return 1
+        elif pick in random_input:
+            return random.randint(0,1)
+        elif not pick or pick not in [player_input,computer_input,random_input]:
+            print(f"if you don't want to pick anything just say {random_input[0]} or {random_input[1]} "
+                  f"but not an empty string")
             print()
-            role = input(f"What do you want to play? ({available_roles})").strip()
-            if role in available_roles:
-                return role
-            else:
-                print(f"that's not a valid move! try any of these {available_roles} inside except")
-        except TypeError:
-            print(f"that's not a valid move! try any of these {available_roles} outside except")
+            continue
+
+
+def make_a_play(roles):
+    role = ''
+    available_roles = [key for key in roles]
+    #TODO: make sure to split this func into 2 for human and computer player
+    while role not in available_roles:
+        print()
+        role = input(f"What do you want to play? ({available_roles})")
+        print(role)
+        if role in available_roles:
+            return role
+        else:
+            print(f"that's not a valid move! try any of these {available_roles} inside except")
+
+
+def toggle_player(player_index):
+    return (player_index + 1) % 2
 
 
 def check_winner():
-    #TODO:check for winner
+    #TODO: make the function to check if there is a winner
     pass
 
 
